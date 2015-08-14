56adce8173053a6e528e958c5acc7ffe65ebc849
V5785 (Course ID) <v5785@cam.ac.uk>
2015-08-14 11:00:37 +0100

    first try

--- drone-backend.js
+++ drone-backend.js
@@ -1,5 +1,4 @@
 var Cylon = require('cylon');
-var bot;
 
 // Initialise the robot
 Cylon.robot()
@@ -11,11 +10,56 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
-    
+
+var bot;
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+
+    bot.drone.config('general:navdata_demo', 'TRUE');
+
+    //bot.nav.on("navdata", function(data) {
+
+    //});
+
+    //bot.nav.on("altitudeChange", function(data) {
+       // console.log("Altitude:", data);
+       // if (altitude > 1) {
+          //  bot.drone.land();
+      //  }
+    //});
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+// Take off
+    bot.drone.takeoff();
+    after(5*1000, function(){
+
+    bot.drone.left(0.2);});
+    after(5*1000, function() {
+            bot.drone.left(0);
+            bot.drone.right(0.2);
+        });
+
+
+
+    after(5*1000, function() {
+            bot.drone.right(0);
+            bot.drone.land();
+            bot.drone.stop();
+
+    });
+
 
 }
+
+
+
+
+
 
 Cylon.start();
